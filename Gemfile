35a823e9436dd4c83d1a6d9cc33c7592f3eec7b7
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-24 21:58:22 +0000

    initial additions

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "rqrcode"
+gem "dotenv"
+gem "pry-byebug"
--- qr.rb
+++ qr.rb
@@ -1,1 +1,18 @@
 # Write your solution here!
+require "rqrcode"
+
+
+
+### For link: # qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
+### For wifi: # qrcode = RQRCode::QRCode.new(WIFI:T:WPA;S:My wifi network;P:supersecretpassword;;)
+### For text message # SMSTO:9876543210:Hi Alice! It's
+
+qrcode = RQRCode::QRCode.new("SMSTO:9492928454:You are soooo good looking")
+
+
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
