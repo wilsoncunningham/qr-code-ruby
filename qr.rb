0f08580fc1bd5918164292a79de05260f7a71b71
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-24 23:11:08 +0000

    finished

--- qr.rb
+++ qr.rb
@@ -2,11 +2,6 @@
 require "rqrcode"
 
 
-
-### For link: # qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
-### For wifi: # qrcode = RQRCode::QRCode.new(WIFI:T:WPA;S:My wifi network;P:supersecretpassword;;)
-### For text message # SMSTO:9876543210:Hi Alice! It's
-
 puts "What kind of QR code would you like to generate?\n
       1.  Open a URL
       2.  Join a wifi network
@@ -14,30 +9,55 @@ puts "What kind of QR code would you like to generate?\n
       
       Press 4 to exit\n\n"
 
-response = gets.chomp
-qr_type = nil
+
+response = nil
+
+until response == "1" || response == "2" || response == "3" ||response == "4" do
+  response = gets.chomp
+  puts "Didn't recognize that selection. Please try again\n\n"
+end
 
 if response == "1"
-  qr_type = "url"
+  puts "\nWhat URL would you like to encode?"
+  url = gets.chomp
+
+  puts "\nWhat would you like to call the PNG?"
+  png_name = gets.chomp
+
+  qrcode = RQRCode::QRCode.new(url)
+
 elsif response == "2"
-  qr_type = "wifi"
+  puts "\nWhat is the name of the wifi network?"
+  network = gets.chomp
+
+  puts "\nWhat is the password?"
+  password = gets.chomp
+
+  puts "\nWhat would you like to call the PNG?"
+  png_name = gets.chomp
+
+  qrcode = RQRCode::QRCode.new("WIFI:T:WPA;S:#{network};P:#{password};;")
+
 elsif response == "3"
-  qr_type = "text"
+  puts "\nWhat is the phone number you want to send a text to?"
+  number = gets.chomp
+
+  puts "\nWhat do you want to populate the message with?"
+  message = gets.chomp
+
+  puts "\nWhat would you like to call the PNG?"
+  png_name = gets.chomp
+
+  qrcode = RQRCode::QRCode.new("SMSTO:#{number}:#{message}")
+
 elsif response == "4"
   puts "Goodbye!"
   exit
-else
-  puts "Didn't recognize that seleection. Please try again\n\n"
 end
-
-puts "The qr type is #{qr_type}"
-
-qrcode = RQRCode::QRCode.new("SMSTO:9492928454:You are soooo good looking")
-
 
 
 # Use the .as_png method to create a 500 pixels by 500 pixels image
-png = qrcode.as_png({ :size => 500 })
+png = qrcode.as_png({:size => 500})
 
 # Write the image data to a file
-IO.binwrite("sometext.png", png.to_s)
+IO.binwrite("#{png_name}.png", png.to_s)
